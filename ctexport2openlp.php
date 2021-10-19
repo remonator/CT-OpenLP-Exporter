f2c972ce32effce0b8af85c75651be9d736d1628
Remo Schneider <remo.schneider@gmx.de>
2021-10-19 17:35:57 +0200

    Update testwise

--- ctexport2openlp.php
+++ ctexport2openlp.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
+//ini_set('display_errors', 1);
+//ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 function build_login_form(){
 	global $error_message;
